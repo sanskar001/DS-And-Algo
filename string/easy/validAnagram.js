0d120f3b6138f546086ab23d765dccb0f2f38ff0
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2024-06-07 16:24:30 +0530

    Create validAnagram.js

---
+++ string/easy/validAnagram.js
@@ -0,0 +1,33 @@
+// Valid Anagram
+
+// Time complexity - O(n) and Space complexity - O(n)
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const sFreq = {};
+  const tFreq = {};
+
+  for (let l of s) {
+    sFreq[l] = (sFreq[l] || 0) + 1;
+  }
+
+  for (let l of t) {
+    tFreq[l] = (tFreq[l] || 0) + 1;
+  }
+
+  for (let key in sFreq) {
+    if (sFreq[key] !== tFreq[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// TESTING
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("tar", "car"));
