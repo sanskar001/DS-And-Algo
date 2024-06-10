ec558902e30a8554b8fd931d92efc5b0ec3fae7e
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2024-06-10 12:57:29 +0530

    Update printKeypadSequence.js

--- string/easy/printKeypadSequence.js
+++ string/easy/printKeypadSequence.js
@@ -10,6 +10,38 @@ Input : HELLO WORLD
 Output : 4433555555666096667775553
 */
 
+/*
+// SOLUTION-1
+
+function printSequence(str) {
+  const keypad = {
+    ABC: "2",
+    DEF: "3",
+    GHI: "4",
+    JKL: "5",
+    MNO: "6",
+    PQRS: "7",
+    TUV: "8",
+    WXYZ: "9",
+    " ": "0",
+  };
+
+  let result = "";
+
+  for (let letter of str) {
+    Object.keys(keypad).forEach((key) => {
+      if (!!key.includes(letter)) {
+        result += keypad[key].repeat(key.indexOf(letter) + 1);
+      }
+    });
+  }
+
+  return result;
+}
+
+
+*/
+
 // const keyMap = {
 //   a: "2",
 //   b: "22",
